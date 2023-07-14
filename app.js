8ff263941d66246a13f27ea35cfc0437dea2e9ee
HorrosAnton <139565793+HorrosAnton@users.noreply.github.com>
2023-07-14 20:47:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+let item = "";
+
+let btn1 = document.getElementBuId("btn1");
+let btn2 = document.getElementBuId("btn2");
+let btn3 = document.getElementBuId("btn3");
+let btn4 = document.getElementBuId("btn4");
+let btn5 = document.getElementBuId("btn5");
+let btn6 = document.getElementBuId("btn6");
+
+btn1.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else{
+        tg.MainButton.setText("Вы выбрали товар 1");
+        item = "1";
+        tg.MainButton.show();
+    }
+})
+
+btn2.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else{
+        tg.MainButton.setText("Вы выбрали товар 2");
+        item = "1";
+        tg.MainButton.show();
+    }
+})
+
+btn3.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else{
+        tg.MainButton.setText("Вы выбрали товар 3");
+        item = "1";
+        tg.MainButton.show();
+    }
+})
+
+btn4.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else{
+        tg.MainButton.setText("Вы выбрали товар 4");
+        item = "1";
+        tg.MainButton.show();
+    }
+})
+
+btn5.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else{
+        tg.MainButton.setText("Вы выбрали товар 5");
+        item = "1";
+        tg.MainButton.show();
+    }
+})
+
+btn6.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else{
+        tg.MainButton.setText("Вы выбрали товар 6");
+        item = "1";
+        tg.MainButton.show();
+    }
+})
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    tg.sendData(item);
+})
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p")
+
+p.innerText = '${tg.initDataUnsafe.first_name}'
+
+usercard.appendChild(p)
