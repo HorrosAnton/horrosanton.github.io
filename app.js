e50edc918c9cebf474bfbee6257564221596c784
HorrosAnton <139565793+HorrosAnton@users.noreply.github.com>
2023-07-14 21:09:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
 
-let item = "";
+let item = " ";
 
 let btn1 = document.getElementBuId("btn1");
 let btn2 = document.getElementBuId("btn2");
@@ -31,7 +31,7 @@ btn2.addEventListener("click", function(){
     }
     else{
         tg.MainButton.setText("Вы выбрали товар 2");
-        item = "1";
+        item = "2";
         tg.MainButton.show();
     }
 })
@@ -42,7 +42,7 @@ btn3.addEventListener("click", function(){
     }
     else{
         tg.MainButton.setText("Вы выбрали товар 3");
-        item = "1";
+        item = "3";
         tg.MainButton.show();
     }
 })
@@ -53,7 +53,7 @@ btn4.addEventListener("click", function(){
     }
     else{
         tg.MainButton.setText("Вы выбрали товар 4");
-        item = "1";
+        item = "4";
         tg.MainButton.show();
     }
 })
@@ -64,7 +64,7 @@ btn5.addEventListener("click", function(){
     }
     else{
         tg.MainButton.setText("Вы выбрали товар 5");
-        item = "1";
+        item = "5";
         tg.MainButton.show();
     }
 })
@@ -75,7 +75,7 @@ btn6.addEventListener("click", function(){
     }
     else{
         tg.MainButton.setText("Вы выбрали товар 6");
-        item = "1";
+        item = "6";
         tg.MainButton.show();
     }
 })
@@ -88,6 +88,7 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p")
 
-p.innerText = '${tg.initDataUnsafe.user.first_name}'
+p.innerText = '${tg.initDataUnsafe.user.first_name}
+    ${tg.initDataUnsafe.user.last_name}';
 
 usercard.appendChild(p)
