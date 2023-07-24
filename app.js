f86f7a256100fa2053dcb939ff45a81682968cb7
HorrosAnton <139565793+HorrosAnton@users.noreply.github.com>
2023-07-24 12:13:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-let tg = window.Telegram.WebApp;
+let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
 
-tg.expand();
+tg.expand(); //расширяем на все окно  
 
-tg.MainButton.text = "Кнопка Cкрытия/Открытия";
-tg.MainButton.setText = "Кнопка поменяла значение Кнопка Cкрытия/Открытия";
-tg.MainButton.textColor ="#F55353";
-tg.MainButton.color = "#143F6B";
-tg.MainButton.setParams({"color": "143F6B"});
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
+tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
 
-btn.addEventListener('click', function(){
-	if (tg.MainButton.isVisible){
-		tg.MainButton.hide()
+btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+	if (tg.MainButton.isVisible){ //если кнопка показана 
+		tg.MainButton.hide() //скрываем кнопку 
 	}
-	else{
-		tg.MainButton.show()
-	}
+  else{ //иначе
+  	tg.MainButton.show() //показываем 
+  }
 });
 
-let btnED = document.getElementById("btnED");
-btnED.addEventListener('click', function(){
-	if (tg.MainButton.asActive){
-		tg.MainButton.setParams({"color": "#222222"});
-		tg.MainButton.disable()
+let btnED = document.getElementById("btnED"); //получаем кнопку активировать/деактивировать
+btnED.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+	if (tg.MainButton.isActive){ //если кнопка показана 
+		tg.MainButton.setParams({"color": "#E0FFFF"}); //меняем цвет
+		tg.MainButton.disable() //скрываем кнопку 
 	}
-	else{
-		tg.MainButton.setParams({"color": "#143F6B"});
-		tg.MainButton.enable()
+	else{ //иначе
+		tg.MainButton.setParams({"color": "#143F6B"}); //меняем цвет
+		tg.MainButton.enable() //показываем 
 	}
 });
 
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
-	tg.sendData("some string");
+	tg.sendData("some string that we need to send"); 
+	//при клике на основную кнопку отправляем данные в строковом виде
 });
 
 
